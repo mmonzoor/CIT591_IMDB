699f11edb1a2311223f52a50b3938a62d98987c0
mmonzoor <mmonzoor.wv@gmail.com>
2019-07-21 16:19:36 -0400

    added search method to create encoded URLs

--- DataParser.java
+++ DataParser.java
@@ -1,4 +1,5 @@
 public class DataParser {
-    
+
+    // take output from MovieaAPIConnection and parse it based on ke response=true. Then input into CSV.
     
 }
--- SearchMovies.java
+++ SearchMovies.java
@@ -1,9 +1,52 @@
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder; 
+
 public class SearchMovies{
-    
+
     // api key - 9d1bb888
     // base url - 
-    SearchMovies(String title, String year){
+    private String title;
+    // optional param
+    private int year = 0;
+    
+    /**
+     * constructor taking in the same search inputs as the user
+     * @param title
+     * @param year
+     */
+    SearchMovies(String title){
         this.title = title;
-        this.year = year;
     }
+
+
+    public String constructSearch(){
+        StringBuilder url = new StringBuilder("http://www.omdbapi.com/?apikey=9d1bb888&type=movie");
+        try {
+            String enTitle = URLEncoder.encode(title, "utf-8");
+            // add encoded title
+            url.append("&t=");
+            url.append(enTitle);
+
+            if(year > 1901 && year < 2020){
+                String yearToString = Integer.toString(year);
+                url.append("&y=");
+                url.append(yearToString);
+            }
+            
+        } catch (UnsupportedEncodingException uee) {
+            uee.printStackTrace();
+        }
+        
+        return url.toString();   
+    }
+    
+    public static void main(String[] args) {
+        int year = 2012;
+        String title = "The Dark Knight Rises";
+
+        SearchMovies sm = new SearchMovies(title);
+        sm.year = year;
+        System.out.println(sm.constructSearch());
+    }
+
 }
