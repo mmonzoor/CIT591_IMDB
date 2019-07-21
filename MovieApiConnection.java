7fa97ab31542b57d0b0cbae49548ee23ce4ee322
mmonzoor <mmonzoor.wv@gmail.com>
2019-07-21 15:02:00 -0400

    added api connector class to fetch data, added gitignore, and created a main SearchMovies clss.

---
+++ MovieApiConnection.java
@@ -0,0 +1,60 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class MovieApiConnection {
+    // base url - 
+    // api key - 9d1bb888
+    String SearchURL;
+    MovieApiConnection(String SearchURL){
+        this.SearchURL = SearchURL;
+    }
+
+    public void requestGET(){
+        try{
+            URL url = new URL(SearchURL);
+            // call open connection to input stram
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // want to perform GET request
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+
+            if(connection.getResponseCode() != 200){
+                throw new IOException();
+            }
+            // read the items from the connection
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            
+            // read the inputs
+            StringBuilder responseStrBuilder = new StringBuilder();
+
+            String inputStr;
+            while ((inputStr = in.readLine()) != null)
+                responseStrBuilder.append(inputStr);
+            //new JSONObject(responseStrBuilder.toString());
+
+            System.out.println(responseStrBuilder.toString());
+            //System.out.println(new JSONObject(responseStrBuilder.toString()));
+            
+        }
+        catch (MalformedURLException mfe) {
+            // case when new URL may have failed
+            // message can be null, so print stack trace for good measures
+            mfe.getMessage();
+            mfe.printStackTrace();
+        }
+        catch (IOException ie){
+            // case when open connection failed
+            ie.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String url = new String("http://www.omdbapi.com/?apikey=9d1bb888&type=movie&t=batman&y=1991");
+        MovieApiConnection c = new MovieApiConnection(url);
+        c.requestGET();
+    }
+
+
+}
---
+++ SearchMovies.java
@@ -0,0 +1,9 @@
+public class SearchMovies{
+    
+    // api key - 9d1bb888
+    // base url - 
+    SearchMovies(String title, String year){
+        this.title = title;
+        this.year = year;
+    }
+}
